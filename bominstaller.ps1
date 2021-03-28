e25bb3713acfe54c2797ab2465ccbf1ff8e01e50
Tim Kopp <timkopp@gmail.com>
2021-03-28 14:36:45 -0400

    powershell installer

---
+++ bominstaller.ps1
@@ -0,0 +1,62 @@
+﻿ 
+function BOMInstaller {
+	[CmdletBinding()]
+	param(
+		[Parameter()]
+		[string] $Version,
+		[Parameter()]
+		[string] $InstallPath,
+		[Parameter()]
+		[bool] $WithUnitTest = $false 
+	) 
+    $v = $PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent
+
+    $BOMURL=''
+    if($Version -notmatch '\d{1,3}\.\d{1,3}' ){
+        $HTML = Invoke-RestMethod 'https://github.com/tmnkopp/BrowseOmatic/releases/latest'
+        $HTML -match '(/tmnkopp/BrowseOmatic/releases/download/.*/BOM.exe)'
+        $BOMURL = "https://github.com" + $Matches[0] 
+        $HTML -match '(/tmnkopp/BrowseOmatic/releases/download/.*/chromedriver.exe)'
+        $CDURL = "https://github.com" + $Matches[0] 
+    }else{ 
+        $BOMURL="https://github.com" + '/tmnkopp/BrowseOmatic/releases/download/' + $Version + '/BOM.exe' 
+    }  
+    if($InstallPath -notmatch '\w{1}:.*\\' ){
+        $InstallPath = 'c:\bom\' 
+    }else{
+        md -Force c:\bom\
+    }
+
+    if( $v ){
+        Write-Host $BOMURL 
+        Write-Host $CDURL
+        Write-Host $InstallPath
+    }
+
+    [System.Environment]::SetEnvironmentVariable('bom', $InstallPath + 'bom.exe',[System.EnvironmentVariableTarget]::User)
+    $webClient = [System.Net.WebClient]::new()
+     
+    try {
+       $WebClient.DownloadFile( $BOMURL , $InstallPath + "BOM.exe"  )
+    }
+    catch [System.Net.WebException],[System.IO.IOException] {
+        "Unable to download bom.exe"
+    } 
+
+    try {
+       $WebClient.DownloadFile( $CDURL  , $InstallPath + "chromedriver.exe"  )
+    }
+    catch [System.Net.WebException],[System.IO.IOException] {
+        "Unable to Download chromedriver"
+    }
+     
+    if($WithUnitTest ){
+        $exe = [System.Environment]::GetEnvironmentVariable('bom', 'User')  
+        & $exe  cmd -t unittest
+    } 
+
+}
+BOMInstaller -Version '~' -WithUnitTest $true
+
+
+
