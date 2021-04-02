ce8c34170bd071f1c879b7c290bbd8fe4d8827d8
Tim Kopp <timkopp@gmail.com>
2021-04-02 10:54:33 -0400

    update unit test

--- PostBuild.ps1
+++ PostBuild.ps1
@@ -4,12 +4,14 @@
     [Parameter(Mandatory = $false, Position = 0)] 
     [string] $m = 'refactor build '  
 )
+
 Write-Host $ConfigurationName
 
 if ($ConfigurationName -match 'Release' ){
     cd 'C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic';    
-    dotnet publish -p:PublishProfile=FolderProfile   
+    dotnet publish BrowseOmatic -p:PublishProfile=FolderProfile   
     Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.exe -Destination c:\bom\BOM.exe -Force 
-    Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\bominstaller.ps1 -Destination c:\bom\bominstaller.ps1 -Force 
-     
+    Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\bominstaller.ps1 -Destination c:\bom\bominstaller.ps1 -Force
+    Remove-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.CORE.pdb -Force
+    Remove-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.pdb -Force 
 }  
