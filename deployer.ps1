f5be304101aeadbd0ac41c5280738ba142444a14
Tim Kopp <timkopp@gmail.com>
2021-04-02 10:44:02 -0400

    refactor build y121O79y121O79

--- deployer.ps1
+++ deployer.ps1
@@ -13,7 +13,9 @@
         dotnet build --configuration Release;
         dotnet publish BrowseOmatic -p:PublishProfile=FolderProfile   
         Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.exe -Destination c:\bom\BOM.exe -Force 
-        Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\bominstaller.ps1 -Destination c:\bom\bominstaller.ps1 -Force 
+        Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\bominstaller.ps1 -Destination c:\bom\bominstaller.ps1 -Force
+        Remove-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.CORE.pdb -Force
+        Remove-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.pdb -Force
         
     }
     if ($with -match ' release | commit ' ){
