150f1b7059995f6e669e9d3b71f8026b7f9d8f70
Tim Kopp <timkopp@gmail.com>
2021-05-04 14:16:32 -0400

    refacor RadFormFill

--- TelerikCommands/RadFormFill.cs
+++ TelerikCommands/RadFormFill.cs
@@ -14,61 +14,57 @@ namespace TelerikCommands
         {
             this.container = Container;
         }
+        private void InputIterator(ISessionDriver dvr, string Selector, Action<string> InputAction) { 
+            List<string> idlist = new List<string>();
+            IList<IWebElement> inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container} {Selector}")); 
+            foreach (IWebElement input in inputs) idlist.Add(input.GetAttribute("id") ?? "");
+            while (idlist.Count > 0)
+            {
+                try
+                {
+                    InputAction(idlist[0]); 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{Selector}: {ex.Message}\n");
+                }
+                idlist.RemoveAt(0);
+            }
+        }
         public void Execute(ISessionContext ctx)
         {
             Random _random = new Random();
             string rnd = DateTime.Now.Day.ToString() + "-" + _random.Next(255).ToString();
             var dvr = ctx.SessionDriver;
-             
+            dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(.5);
+
+            InputIterator(dvr, " input[id*='MultiSelect_Input']", (s) => { 
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+                IList<IWebElement> options = dvr.Driver.FindElements(By.CssSelector($".rcbSlide .rcbList li"));
+                if (options.Count > 1) options[1].Click();
+            });
+            InputIterator(dvr, " input[type='radio']", (s) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+                if (input.GetAttribute("value") != "") input.Click();
+            });
+            InputIterator(dvr, " input[type='radio']", (s) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+                if (input.GetAttribute("value") == "Y") input.Click();
+            }); 
+            InputIterator(dvr, " .RadDropDownList", (s) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+                input.Click();
+                dvr.Pause(250).Click("ul[class*='rddlList'] li:nth-child(2)").Click("body").Pause(50);
+            });
+            InputIterator(dvr, " .RadComboBox", (s) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+                input.Click();
+                dvr.Pause(450).Click("ul[class*='rcbList'] li:nth-child(2)").Click("body").Pause(150);
+            });
             IList<IWebElement> inputs;
+            List<string> idlist = new List<string>(); 
             try
-            {
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container} input[id*='MultiSelect_Input']"));
-                dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
-                List<string> ids = new List<string>();
-                foreach (IWebElement input in inputs) ids.Add(input.GetAttribute("id") ?? "");
-                foreach (string id in ids)
-                {
-                    IWebElement el = dvr.Driver.FindElement(By.CssSelector($"#{id}"));
-                    el.Click();
-                    dvr.Pause(450);
-                    IList<IWebElement> options = dvr.Driver.FindElements(By.CssSelector($".rcbSlide .rcbList li"));
-                    if (options.Count > 1)
-                    { 
-                        int index = _random.Next(1, options.Count - 1);
-                        options[2].Click(); 
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"sections.SelectByIndex index out of range {ex.Message}");
-            } 
-            try
-            {
-
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  input[type='radio']"));
-                foreach (var input in inputs) { 
-                    if (input.GetAttribute("value") == "Y") input.Click();
-                } 
-
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  .RadDropDownList"));
-                foreach (var input in inputs)
-                {
-                    input.Click();
-                    dvr.Pause(250).Click("ul[class*='rddlList'] li:nth-child(2)").Click("body").Pause(50);
-                } 
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  .RadDropDownList"));
-                foreach (var input in inputs) { 
-                    input.Click();
-                    dvr.Pause(250).Click("ul[class*='rddlList'] li:nth-child(2)").Click("body").Pause(50);
-                }
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  .RadComboBox"));
-                foreach (var input in inputs)
-                {
-                    input.Click();
-                    dvr.Pause(450).Click("ul[class*='rcbList'] li:nth-child(2)").Click("body").Pause(150);
-                } 
+            { 
                 inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  input[id*='date']"));
                 foreach (var input in inputs)
                     if (input.GetAttribute("value") == "") input.SendKeys($"{DateTime.Now.ToShortDateString()}");
