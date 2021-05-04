73249081b47db4913759dcdfa37fc9b4f2ca3131
Tim Kopp <timkopp@gmail.com>
2021-05-04 14:30:45 -0400

    refacor FormFill

--- TelerikCommands/RadFormFill.cs
+++ TelerikCommands/RadFormFill.cs
@@ -38,29 +38,34 @@ namespace TelerikCommands
             var dvr = ctx.SessionDriver;
             dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(.5);
 
-            InputIterator(dvr, " input[id*='MultiSelect_Input']", (s) => { 
-                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+            InputIterator(dvr, " input[id*='MultiSelect_Input']", (inputid) => { 
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{inputid}"));
                 IList<IWebElement> options = dvr.Driver.FindElements(By.CssSelector($".rcbSlide .rcbList li"));
                 if (options.Count > 1) options[1].Click();
             });
-            InputIterator(dvr, " input[type='radio']", (s) => {
-                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+            InputIterator(dvr, " input[type='radio']", (inputid) => {
+                IWebElement input = dvr.Pause(150).Driver.FindElement(By.CssSelector($"#{inputid}"));
                 if (input.GetAttribute("value") != "") input.Click();
             });
-            InputIterator(dvr, " input[type='radio']", (s) => {
-                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+            InputIterator(dvr, " input[type='radio']", (inputid) => {
+                IWebElement input = dvr.Pause(150).Driver.FindElement(By.CssSelector($"#{inputid}"));
                 if (input.GetAttribute("value") == "Y") input.Click();
             }); 
-            InputIterator(dvr, " .RadDropDownList", (s) => {
-                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+            InputIterator(dvr, " .RadDropDownList", (inputid) => {
+                IWebElement input = dvr.Pause(150).Driver.FindElement(By.CssSelector($"#{inputid}"));
                 input.Click();
                 dvr.Pause(250).Click("ul[class*='rddlList'] li:nth-child(2)").Click("body").Pause(50);
             });
-            InputIterator(dvr, " .RadComboBox", (s) => {
-                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{s}"));
+            InputIterator(dvr, " .RadComboBox", (inputid) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{inputid}"));
                 input.Click();
                 dvr.Pause(450).Click("ul[class*='rcbList'] li:nth-child(2)").Click("body").Pause(150);
             });
+            InputIterator(dvr, "textarea", (inputid) => {
+                IWebElement input = dvr.Pause(250).Driver.FindElement(By.CssSelector($"#{inputid}"));
+                if (input.GetAttribute("value") == "") input.SendKeys($"{input.GetAttribute("id")}");
+            });
+
             IList<IWebElement> inputs;
             List<string> idlist = new List<string>(); 
             try
@@ -68,20 +73,21 @@ namespace TelerikCommands
                 inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  input[id*='date']"));
                 foreach (var input in inputs)
                     if (input.GetAttribute("value") == "") input.SendKeys($"{DateTime.Now.ToShortDateString()}");
-
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  input[type='text']"));
-                foreach (var input in inputs)
-                    if (input.GetAttribute("value") == "") input.SendKeys($"0");
-
-                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  .RadGrid textarea"));
-                foreach (var input in inputs)
-                    if (input.GetAttribute("value") == "") input.SendKeys($"{rnd}");
-                 
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"RadFormFill: {ex.Message}\n");
+                Console.WriteLine($"RadFormFill input[id*='date']: {ex.Message}\n");
             }
+            try
+            {
+                inputs = dvr.Driver.FindElements(By.CssSelector($"{this.container}  input[type='text']"));
+                foreach (var input in inputs)
+                    if (input.GetAttribute("value") == "") input.SendKeys($"0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"RadFormFill input[type='text']: {ex.Message}\n");
+            } 
         }
     }
 }
