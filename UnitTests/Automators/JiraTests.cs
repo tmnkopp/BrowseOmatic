0e184dfcfc8a188a40be237e8a202dc645334042
Tim Kopp <timkopp@gmail.com>
2021-08-10 11:39:52 -0400

    ICommand   overrides

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -36,18 +36,17 @@ namespace UnitTests
             var ctx = Session.Context("jira");
             var dvr = ctx.SessionDriver;
             dvr.Pause(1000);//|.*Prepopulation.*  .*BOD.*Section.*[1-3].*|.*CSHELP-2899
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*");
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*Database.*");
             urlProvider.Execute(ctx); 
             foreach (KeyValuePair<string,string> kvp in urlProvider.Hrefs)
             {
-                dvr.Pause(550).GetUrl(kvp.Key);
                 dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
-                //dvr.Pause(550).Click("a[title*='Start Progress']").Pause(550);
-
-                dvr.Pause(100).Click("opsbar-operations_more");
-                dvr.Pause(100).Click("log-work");
-                dvr.Pause(100).SendKeys("input[id='log-work-time-logged']", "10m");
-                dvr.Pause(100).Click("input[id='log-work-submit']");
+                dvr.Pause(550).GetUrl(kvp.Key); 
+                //dvr.Pause(550).Click("a[title*='Start Progress']").Pause(550); 
+                dvr.Click("opsbar-operations_more");
+                dvr.Click("log-work");
+                dvr.SendKeys("input[id='log-work-time-logged']", "10m");
+                dvr.Click("input[id='log-work-submit']");
 
                 // dvr.Pause(1200).Click("a[title*='Resolve']");    
                 // dvr.Pause(350).Click("input[id*='issue-workflow-transition-submit']"); 
