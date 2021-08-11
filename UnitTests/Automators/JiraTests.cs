3f00bd34a5bded1c6cd8b5f5d0285e48c8cae82d
Tim Kopp <timkopp@gmail.com>
2021-08-11 12:00:11 -0400

    updated unit tests

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -5,6 +5,8 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace UnitTests
@@ -12,6 +14,7 @@ namespace UnitTests
     [TestClass]
     public class JiraTests
     {
+        StringBuilder sb = new StringBuilder();
         [TestMethod]
         public void URL_Logger()
         {
@@ -36,16 +39,16 @@ namespace UnitTests
             var ctx = Session.Context("jira");
             var dvr = ctx.SessionDriver;
             dvr.Pause(1000);//|.*Prepopulation.*  .*BOD.*Section.*[1-3].*|.*CSHELP-2899
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*Database.*");
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*Database.*|.*Finding.*");
             urlProvider.Execute(ctx); 
             foreach (KeyValuePair<string,string> kvp in urlProvider.Hrefs)
-            {
-                dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            { 
                 dvr.Pause(550).GetUrl(kvp.Key); 
+                dvr.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
                 //dvr.Pause(550).Click("a[title*='Start Progress']").Pause(550); 
-                dvr.Click("opsbar-operations_more");
-                dvr.Click("log-work");
-                dvr.SendKeys("input[id='log-work-time-logged']", "10m");
+                dvr.Click("opsbar-operations_more"); 
+                dvr.Click("log-work"); 
+                dvr.SendKeys("input[id='log-work-time-logged']", "15m");
                 dvr.Click("input[id='log-work-submit']");
 
                 // dvr.Pause(1200).Click("a[title*='Resolve']");    
@@ -53,6 +56,8 @@ namespace UnitTests
                 // dvr.Pause(350).Click("a[title*='Ready To Test']");
                 // dvr.Pause(350).Click("input[id*='issue-workflow-transition-submit']");
             }
+            var s = sb.ToString();
+            File.WriteAllText($"c:\\bom\\unittest\\jira.yaml", s, Encoding.Unicode);
             dvr.Dispose();
         }
     }
