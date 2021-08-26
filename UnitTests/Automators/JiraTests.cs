685c9955ca07f01c74db83c99cebcc7b84953d0d
Tim Kopp <timkopp@gmail.com>
2021-08-26 08:21:53 -0400

    refactor unit tests for serializer 6697

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -36,29 +36,49 @@ namespace UnitTests
             CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(task);
 
-            WriteTasks(tasks);
+            WriteTasks(tasks );
         }       
         [TestMethod]
         public void JiraIssue_List()
         { 
-            BTask task = new BTask("taketime", "jira"); 
+            BTask task = new BTask("export_all", "jira"); 
             task.TaskSteps.Add(new TaskStep("Url", new string[] { "https://dayman.cyber-balance.com/jira/issues/?jql=project%20%3D%20CS%20AND%20assignee%20%3D%20currentUser()%20AND%20type%20%3DTask%20AND%20resolution%20%3D%20Unresolved" }));
             task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "header-operations" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "allCsvFields" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "csv-export-dialog-export-button" }));
-            
+            task.TaskSteps.Add(new TaskStep("Click", new string[] { "csv-export-dialog-export-button" })); 
             task.TaskSteps.Add(new TaskStep("Script", new string[] {  @" 
-            document.title='TASKS';
-            window.scrollTo(100, 1000); 
-            " }));
- 
-            tasks.Add(task);
-      
+                document.title='TASKS';
+                window.scrollTo(100, 1000); 
+            " })); 
+            tasks.Add(task); 
             CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(task);
 
             WriteTasks(tasks);
+        }
+        [TestMethod]
+        public void JiraTime_Taker()
+        {
+            var ctx = Session.Context("jira");
+            var dvr = ctx.SessionDriver; //|.*Prepopulation.*  .*BOD.*Section.*[1-3].*|.*CSHELP-2899
+            ctx.SessionDriver.Connect(ctx.configContext.conn);
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*Section.*");
+            urlProvider.Execute(ctx);
+            BTask task = new BTask("saop_taketime", "jira");
+            foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
+            { 
+                task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
+                task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
+                task.TaskSteps.Add(new TaskStep("Click", new string[] { "opsbar-operations_more" }));
+                task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
+                task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "5m" }));
+                task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1500" })); 
+            }
+            tasks.Add(task);
+            WriteTasks(tasks);
+            dvr.Dispose();
         }
         [TestMethod]
         public void JiraIssue_Starter()
@@ -71,11 +91,7 @@ namespace UnitTests
             {
                 dvr.GetUrl(kvp.Key); 
                 ctx.SessionDriver.Timeout = 2;
-                //dvr.Pause(550).Click("a[title*='Start Progress']").Pause(550);
-                dvr.Click("opsbar-operations_more"); 
-                dvr.Click("log-work"); 
-                dvr.Pause(900).SendKeys("input[id='log-work-time-logged']", "15m"); 
-                dvr.Pause(1200).Click("input[id='log-work-submit']").Pause(1200);
+                //dvr.Pause(550).Click("a[title*='Start Progress']").Pause(550); 
                 // dvr.Click("a[title*='Resolve']")
                 // .Click("input[id*='issue-workflow-transition-submit']")
                 // .Click("a[title*='Ready To Test']")
@@ -83,10 +99,11 @@ namespace UnitTests
             }  
             dvr.Dispose();
         }
-        private void WriteTasks(List<BTask> tasks)
+        private void WriteTasks(List<BTask> tasks )
         {
             var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-            File.WriteAllText($"c:\\bom\\unittest\\output.yaml", serializer.Serialize(tasks), Encoding.Unicode);
+            File.WriteAllText($"c:\\bom\\unittest\\{tasks[0].Context}_{tasks[0].Name}.yaml", serializer.Serialize(tasks), Encoding.Unicode);
+            File.WriteAllText($"c:\\bom\\unittest\\{tasks[0].Context}_{tasks[0].Name}.ps1", $"bom run -t {tasks[0].Name} -k -p c:\\bom\\unittest\\{tasks[0].Context}_{tasks[0].Name}.yaml", Encoding.Unicode);
         }
     }
   
