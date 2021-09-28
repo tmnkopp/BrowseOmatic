4e065378eb725406481174db63656253f6bfa801
Tim Kopp <timkopp@gmail.com>
2021-09-28 09:04:17 -0400

    add SetOptionByTextO79Y89

---
+++ BOM.CORE/Command/SetOptionByText.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BOM.CORE
+{
+    public class SetOptionByText : ICommand
+    {
+        private string Element = "";
+        private string option = ".*";
+        public SetOptionByText( string Element, string Option)
+        {
+            this.Element = Element;
+            this.option = Option;
+        } 
+        public void Execute(ISessionContext ctx)
+        {
+            var dvr = ctx.SessionDriver;
+            ctx.SessionDriver.Pause(0);
+            SelectElement sections = new SelectElement(dvr.Select(Element));
+            if (sections != null)
+            {
+                try
+                {
+                    ctx.SessionDriver.Log.LogInformation("SetOptionByText {o}", Element); 
+                    sections?.Options.Where(o => o.Text.Contains(this.option)).FirstOrDefault()?.Click();
+                }
+                catch (Exception e)
+                {
+                    ctx.SessionDriver.Log.LogWarning("SetOptionByText {o}", e.Message); 
+                }
+            }  
+        }
+    }
+}
--- deployer.ps1
+++ deployer.ps1
@@ -19,17 +19,17 @@
         dotnet build --configuration Release;
         dotnet publish BrowseOmatic -p:PublishProfile=FolderProfile   
         Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.exe -Destination c:\bom\BOM.exe -Force 
-        Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.exe -Destination D:\dev\CyberScope\CyberScopeBranch\CSwebdev\test\bom\BOM.exe -Force 
+        # Copy-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\BOM.exe -Destination D:\dev\CyberScope\CyberScopeBranch\CSwebdev\test\bom\BOM.exe -Force 
  
         Remove-Item -Path C:\Users\Tim\source\repos\BrowseOmatic\BrowseOmatic\bin\publish\*.pdb -Force 
     }
     if ($with -match ' commit ' ){
         $m = -join ((65..90) + (97..122) | Get-Random -Count 2 | % {[char]$_ +''+ $_ })
-        $message = 'add context command line arg i-' + $m   
+        $message = 'add SetOptionByText' + $m   
         cd 'C:\Users\Tim\source\repos\BrowseOmatic';  
         git add .; git commit -m $message; git push; 
     }  
-    explorer.exe d:\dev\CyberScope\CyberScopeBranch\CSwebdev\test\bom
-    # explorer.exe C:\BOM commit
+    # explorer.exe d:\dev\CyberScope\CyberScopeBranch\CSwebdev\test\bom
+    explorer.exe C:\BOM commit
 } 
 Invoke-BOM-Workflow -with " release commit  " 
