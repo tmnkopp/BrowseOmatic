dbc44fc9b8dc2d140157decf840fa1e1f027c782
Tim Kopp <timkopp@gmail.com>
2021-11-09 08:19:31 -0500

    add alert acceptE69q113

---
+++ BOM.CORE/Command/Accept.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOM.CORE 
+{  
+    public class AlertAccept : ICommand
+    {
+        int WaitTime = 0; 
+        public AlertAccept(string pause)
+        {
+            this.WaitTime = Convert.ToInt32(pause ?? "0"); 
+        } 
+        public void Execute(ISessionContext ctx)
+        {
+            var driver = ctx.SessionDriver.Driver;
+            IAlert alert = null; 
+            WebDriverWait wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(WaitTime)); 
+            try
+            {
+                alert = wait.Until(d =>
+                {
+                    try
+                    { 
+                        return driver.SwitchTo().Alert();
+                    }
+                    catch (NoAlertPresentException)
+                    { 
+                        return null;
+                    }
+                });
+            }
+            catch (WebDriverTimeoutException) { alert = null; } 
+        }
+    } 
+}
