2557d08ce181a8815529df4b524cc5f693b84da6
Tim Kopp <timkopp@gmail.com>
2021-11-16 07:41:30 -0500

    refactor config 102106

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -78,18 +78,18 @@ namespace UnitTests
         public void JiraTime_Taker()
         {
             var ctx = Session.Context("jira");
-            var dvr = ctx.SessionDriver; //|.*Prepopulation.*  .*BOD.*Section.*[1-3].*|.*CSHELP-2899
+            var dvr = ctx.SessionDriver; 
             ctx.SessionDriver.Connect(ctx.configContext.conn);
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*SAOP.*Section.*");
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-85']", ".*EINSTEIN.*");
             urlProvider.Execute(ctx);
-            BTask task = new BTask("saop_taketime", "jira");
+            BTask task = new BTask("ein_taketime", "jira");
             foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
             { 
                 task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
                 task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "opsbar-operations_more" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
-                task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "5m" }));
+                task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "20m" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
                 task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1500" })); 
             }
