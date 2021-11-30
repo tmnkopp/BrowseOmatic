2fd281d1ba460be4f4e5e786edfaa127ded893b1
Tim Kopp <timkopp@gmail.com>
2021-11-30 13:57:09 -0500

    refactor config 97101

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -32,11 +32,9 @@ namespace UnitTests
             task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "15m" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
             task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" })); 
-            tasks.Add(task);
-      
-            CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
-            processor.Process(task);
-
+            tasks.Add(task); 
+            //CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
+            //processor.Process(task); 
             WriteTasks(tasks);
         }       
         [TestMethod]
@@ -44,7 +42,6 @@ namespace UnitTests
         { 
             BTask task = new BTask("export_all", "jira"); 
             task.TaskSteps.Add(new TaskStep("Url", new string[] { "https://dayman.cyber-balance.com/jira/issues/?jql=project%20%3D%20CS%20AND%20assignee%20%3D%20currentUser()%20AND%20type%20%3DTask%20AND%20resolution%20%3D%20Unresolved" }));
-            task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "header-operations" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "allCsvFields" }));
             task.TaskSteps.Add(new TaskStep("Click", new string[] { "csv-export-dialog-export-button" })); 
@@ -68,9 +65,8 @@ namespace UnitTests
                 window.scrollTo(100, 1000); 
             " }));
             tasks.Add(task);
-            CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
-            processor.Process(task);
-
+            //CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
+            //processor.Process(task); 
             WriteTasks(tasks);
         }
 
@@ -86,18 +82,12 @@ namespace UnitTests
             foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
             { 
                 task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
-                task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
+                task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "1" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "opsbar-operations_more" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
-                task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "25m" }));
+                task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "15m" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1500" }));
-                // task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Resolve Issue']" }));
-                // task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
-                // task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1500" }));
-                // task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Ready To Test']" }));
-                // task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
-                // task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1500" }));
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" })); 
                 tasks.Add(task); 
             }
             
