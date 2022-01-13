634d4b56c28a2f7d97532a66fe252e402ac7a9a8
Tim Kopp <timkopp@gmail.com>
2022-01-13 09:41:22 -0500

    refactor unit tests 101105

--- UnitTests/CommandTests.cs
+++ UnitTests/CommandTests.cs
@@ -76,28 +76,6 @@ namespace UnitTests
             var st = $"{vals[0]}{sb.ToString()}";
             Assert.IsNotNull(st);
         }
-         
-        [TestMethod]
-        public void OpenTab_Opens()
-        {
-            var config = new TestServices().Configuration; 
-            ILogger<ContextProvider> logger = new Mock<ILogger<ContextProvider>>().Object;
- 
-            ICommand cmd = new OpenTab("https://gist.github.com/safebear/a550c4094811993f3c223e1d2f8a8eb5");
-            ICommand url = new Url("https://gist.github.com/safebear/a550c4094811993f3c223e1d2f8a8eb5");
-            ISessionDriver dvr = new SessionDriver(
-                config
-                , logger 
-                ); 
-            ISessionContext ctx = new SessionContext();
-            ctx.Name = "unittest";
-            ctx.SessionDriver = dvr;
-            ctx.SessionDriver.Create();
-            url.Execute(ctx);
-            cmd.Execute(ctx);
-
-            Assert.IsNotNull(ctx);
-
-        }
+          
     }
 }
