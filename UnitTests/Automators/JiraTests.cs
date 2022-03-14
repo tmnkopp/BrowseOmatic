381664b8ac295baba3db25e6ffc37cfb5532bb9c
Tim Kopp <timkopp@gmail.com>
2022-03-14 13:12:18 -0400

    unit tests refac9069

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -79,18 +79,18 @@ namespace UnitTests
             CommandProcessor processor = new CommandProcessor(ctx, new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(ctx.ContextConfig.conntask);
 
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-86']", ".*");
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*Database Script.*");
             urlProvider.Execute(ctx);
             BTask task = new BTask("taketime", "jira");
             foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
             { 
-                task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
+                task.TaskSteps.Add(new TaskStep("OpenTab", new string[] { kvp.Key }));
                 task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "1" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "opsbar-operations_more" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
                 task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "15m" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "15500" })); 
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "10500" })); 
                 tasks.Add(task); 
             } 
              processor.Process(task);
@@ -101,25 +101,31 @@ namespace UnitTests
         public void JiraIssue_Closer()
         {
             var ctx = Session.Context("jira");
-            var dvr = ctx.SessionDriver; //|.*Prepopulation.*  .*BOD.*Section.*[1-3].*|.*CSHELP-2899
+            var dvr = ctx.SessionDriver;
             ctx.SessionDriver.Create();
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*CIO|EINSTEIN|BOD.*");
+
+            CommandProcessor processor = new CommandProcessor(ctx, new Mock<ILogger<ContextProvider>>().Object);
+            processor.Process(ctx.ContextConfig.conntask);
+
+            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*Database Script.*");
             urlProvider.Execute(ctx);
-            BTask task = new BTask("resolve_tickets", "jira");
+            BTask task = new BTask("startprog_tickets", "jira");
             foreach (KeyValuePair<string,string> kvp in urlProvider.Items)
             { 
                 task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
                 task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1200" }));
-                task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Resolve']" }));
-                task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" })); 
-                task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Ready To Test']" }));
-                task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
+                task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Start Progress']" }));
+
+                //  task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Resolve']" }));
+                //  task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" })); 
+                //  task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Ready To Test']" }));
+                //  task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
                 task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" }));
                 tasks.Add(task);
             }
-            CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(task);
-            Utils.WriteTasks(tasks);
+
+            //Utils.WriteTasks(tasks);
             //dvr.Dispose();
         }
         [TestMethod]
