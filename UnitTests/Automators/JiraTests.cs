b079e190fc1f72fa722cb85b74628004b05c42d3
Tim Kopp <timkopp@gmail.com>
2022-04-13 13:16:38 -0400

    unit tests refactor 6984

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -20,66 +20,25 @@ namespace UnitTests
     [TestClass]
     public class JiraTests
     {
-        CommandProcessor processor; 
+        CommandProcessor processor;
+        private string selector;
+        private string matchPattern;
         List<BTask> tasks = new List<BTask>();
-        [TestMethod]
-        public void JiraIssue_TimeTaker()
-        { 
-            BTask task = new BTask("taketime", "jira");  
-            task.TaskSteps.Add(new TaskStep("Url", new string[] { "https://dayman.cyber-balance.com/jira/browse/CS-8450" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "opsbar-operations_more" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
-            task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "15m" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
-            task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" })); 
-            tasks.Add(task); 
-            //CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
-            //processor.Process(task); 
-            Utils.WriteTasks(tasks);
-        }       
-        [TestMethod]
-        public void JiraIssue_List()
-        { 
-            BTask task = new BTask("export_all", "jira"); 
-            task.TaskSteps.Add(new TaskStep("Url", new string[] { "https://dayman.cyber-balance.com/jira/issues/?jql=project%20%3D%20CS%20AND%20assignee%20%3D%20currentUser()%20AND%20type%20%3DTask%20AND%20resolution%20%3D%20Unresolved" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "header-operations" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "allCsvFields" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "csv-export-dialog-export-button" })); 
-            task.TaskSteps.Add(new TaskStep("Script", new string[] {  @" 
-                document.title='TASKS';
-                window.scrollTo(100, 1000); 
-            " })); 
-            tasks.Add(task); 
-            //CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
-            //processor.Process(task); 
-            Utils.WriteTasks(tasks);
-        }
-        [TestMethod]
-        public void JiraCreate()
+        public JiraTests()
         {
-            BTask task = new BTask("create", "jira");
-            task.TaskSteps.Add(new TaskStep("SetWait", new string[] { "2" }));
-            task.TaskSteps.Add(new TaskStep("Click", new string[] { "create_link" })); 
-            task.TaskSteps.Add(new TaskStep("Script", new string[] { @" 
-                document.title='TASKS';
-                window.scrollTo(100, 1000); 
-            " })); 
-            //CommandProcessor processor = new CommandProcessor(Session.Context(task.Context), new Mock<ILogger<ContextProvider>>().Object);
-            //processor.Process(task); 
-            Utils.WriteTask(task);
+            selector = ".issue-table tr .summary a[href*='browse/CS-88']"; 
+            matchPattern = ".*Annual IG.*";
         }
-
         [TestMethod]
         public void JiraTime_Taker()
         {
             var ctx = Session.Context("jira");
             var dvr = ctx.SessionDriver; 
-            ctx.SessionDriver.Create();
-
+            ctx.SessionDriver.Create(); 
             CommandProcessor processor = new CommandProcessor(ctx, new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(ctx.ContextConfig.conntask);
 
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*Database Script.*");
+            var urlProvider = new UrlProvider(selector, matchPattern);
             urlProvider.Execute(ctx);
             BTask task = new BTask("taketime", "jira");
             foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
@@ -90,12 +49,11 @@ namespace UnitTests
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "log-work" }));
                 task.TaskSteps.Add(new TaskStep("Key", new string[] { "input[id='log-work-time-logged']", "15m" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id='log-work-submit']" }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "2500" })); 
-       
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "2500" }));  
             } 
             processor.Process(task);
             Utils.WriteTask(task);
-            //dvr.Dispose(); 
+            dvr.Dispose(); 
         }
         [TestMethod]
         public void JiraIssue_Opener()
@@ -107,7 +65,7 @@ namespace UnitTests
             CommandProcessor processor = new CommandProcessor(ctx, new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(ctx.ContextConfig.conntask);
 
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*Database Script.*");
+            var urlProvider = new UrlProvider(selector, matchPattern);
             urlProvider.Execute(ctx);
             BTask task = new BTask("startprog_tickets", "jira");
             foreach (KeyValuePair<string, string> kvp in urlProvider.Items)
@@ -132,23 +90,23 @@ namespace UnitTests
             CommandProcessor processor = new CommandProcessor(ctx, new Mock<ILogger<ContextProvider>>().Object);
             processor.Process(ctx.ContextConfig.conntask);
 
-            var urlProvider = new UrlProvider(".issue-table tr .summary a[href*='browse/CS-8']", ".*Database Script.*");
+            var urlProvider = new UrlProvider(selector, matchPattern);
             urlProvider.Execute(ctx);
             BTask task = new BTask("startprog_tickets", "jira");
             foreach (KeyValuePair<string,string> kvp in urlProvider.Items)
             { 
                 task.TaskSteps.Add(new TaskStep("Url", new string[] { kvp.Key }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" }));  
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1200" }));  
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Resolve']" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" }));
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1200" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "a[title*='Ready To Test']" }));
                 task.TaskSteps.Add(new TaskStep("Click", new string[] { "input[id*='issue-workflow-transition-submit']" }));
-                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "900" }));
+                task.TaskSteps.Add(new TaskStep("Pause", new string[] { "1200" }));
                 tasks.Add(task);
             }
             processor.Process(task);
-            Utils.WriteTask(task);
+            //Utils.WriteTask(task);
             //dvr.Dispose();
         }
         [TestMethod]
