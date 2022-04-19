ae6e51f0294272c87d6c113cd7dab277fd145594
Tim Kopp <timkopp@gmail.com>
2022-04-19 16:54:12 -0400

    unit tests refac106108

--- UnitTests/Automators/JiraTests.cs
+++ UnitTests/Automators/JiraTests.cs
@@ -26,8 +26,8 @@ namespace UnitTests
         List<BTask> tasks = new List<BTask>();
         public JiraTests()
         {
-            selector = ".issue-table tr .summary a[href*='browse/CS-88']"; 
-            matchPattern = ".*Annual IG.*";
+            selector = ".issue-table tr .summary a[href*='browse/CS-8']"; 
+            matchPattern = ".*Annual IG Section.*";
         }
         [TestMethod]
         public void JiraTime_Taker()
